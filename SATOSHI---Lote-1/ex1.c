50665bb5508675c4cc01d1ad7c254d85bc290c3c
fatecturmonatop <henryspam1@hotmail.com>
2016-08-05 23:54:09 -0300

    Update ex1.c

--- SATOSHI---Lote-1/ex1.c
+++ SATOSHI---Lote-1/ex1.c
@@ -1,6 +1,6 @@
 // Alguem cola ai vlw flws (precisava de pelo menos 1 arquivo na pasta pro Git funfar)
 // pere
-
+// os exercicio tá bem simples, se quiserem deixar ele mais denso, sempre é bom né guys?
 #include <stdio.h>
 #include <conio.h>
 

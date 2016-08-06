8e5a5925f83cc1b0877981982f33909e618774a1
fatecturmonatop <henryspam1@hotmail.com>
2016-08-05 23:53:26 -0300

    Update ex1.c

--- SATOSHI---Lote-1/ex1.c
+++ SATOSHI---Lote-1/ex1.c
@@ -1,1 +1,22 @@
 // Alguem cola ai vlw flws (precisava de pelo menos 1 arquivo na pasta pro Git funfar)
+// pere
+
+#include <stdio.h>
+#include <conio.h>
+
+
+//Vitor de França
+
+main() {
+	
+	int ladoA, area;
+	
+	printf("Digite o lado do quadrado\n: ");
+	scanf("%i", &ladoA);
+	
+	area = ladoA * ladoA;
+	
+	printf("A area do quadrado equivale a: %i", area);
+	
+	getch();
+}

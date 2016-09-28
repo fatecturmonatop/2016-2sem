7f0eb024b2c66a4b383d2de9f84a2350cad6ea51
henry701 <henry701@users.noreply.github.com>
2016-09-28 08:26:27 -0300

    Update explicacao_ponteiros.c

--- Linguagem De Programacao/explicacao_ponteiros.c
+++ Linguagem De Programacao/explicacao_ponteiros.c
@@ -10,24 +10,24 @@ int main()
 	printf("Insira o tamanho do vetor: ");
 	scanf("%i", &y);
 	
-	// A funÁ„o malloc aloca a quantidade de bytes que lhe È passada como argumento.
+	// A fun√ß√£o malloc aloca a quantidade de bytes que lhe √© passada como argumento.
 	// A linha abaixo aloca um tamanho de: y * tamanho_de(tipo inteiro).
 	// Fala para o compilador que o que foi alocado vai ser um ponteiro pra um valor inteiro (int*)
-	// O compilador n„o reconhece como matriz e nem sabe qual o tamanho da matriz, mas como a matriz
-	// n„o È nada mais do que memÛria que est· "do lado" da outra, qualquer soma dentro do tamanho
-	// dessa matriz vai funcionar, na pr·tica, como uma matriz.
+	// O compilador n√£o reconhece como matriz e nem sabe qual o tamanho da matriz, mas como a matriz
+	// n√£o √© nada mais do que mem√≥ria que est√° "do lado" da outra, qualquer soma dentro do tamanho
+	// dessa matriz vai funcionar, na pr√°tica, como uma matriz.
 	x = (int*) malloc(y * sizeof(int));
 	
-	// Essa linha atribui o valor 5 no Ìndice 2 da matriz.
+	// Essa linha atribui o valor 5 no √≠ndice 2 da matriz.
 	*(x + 2 * sizeof(int)) = 5;
 	
-	// Essa tambÈm. O compilador aceita essa notaÁ„o, e na pr·tica, ele converte isso pra soma que est· l· em cima.
-	x[2] = 5;
+	// Essa tamb√©m. O compilador aceita essa nota√ß√£o, e na pr√°tica, ele converte isso pra soma que est√° l√° em cima.
+	x[2] = 5; 
 	
 	// Importante: Caso o valor inserido para a matriz seja menos de 3, o programa vai apresentar
-	// erro fatal nas duas linhas anteriores, pois o valor n„o foi alocado para a matriz mas est· sendo usado.
-	// E caso n„o apresente erro fatal, outra parte da memÛria do programa foi alterada, e isso pode vir a causar
-	// erros no futuro, ou corrupÁ„o de dados do programa.
+	// erro fatal nas duas linhas anteriores, pois o valor n√£o foi alocado para a matriz mas est√° sendo usado.
+	// E caso n√£o apresente erro fatal, outra parte da mem√≥ria do programa foi alterada, e isso pode vir a causar
+	// erros no futuro, ou corrup√ß√£o de dados do programa.
 	
 	printf("Valor de x[2]: %i\n", x[2]);
 	
